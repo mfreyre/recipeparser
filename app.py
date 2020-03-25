5e71819d19e1b46c6c6d20c8ebce8e450ed7e9bc
mfreyre <mfreyre@etsy.com>
2020-03-25 19:04:51 -0400

    read in file content

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ def main():
     print "hello world"
     filename = raw_input("please enter filename:")
     print filename
+    f = open(filename, "r")
+    contents = list(f)
+    f.close()
+    for line in contents:
+        print line
+
 
 
 
