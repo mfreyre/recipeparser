07cd33845df3eafdc7f763308a10d6bbb34f2423
mfreyre <mfreyre@etsy.com>
2020-03-25 21:36:36 -0400

    add some computer parsing help

--- recipe_app.py
+++ recipe_app.py
@@ -14,19 +14,47 @@ def main():
     for line in contents:
         wordlist = line.split()
         print line
-        amt_idx = int(raw_input("where does the amount break go? "))
-        amt = " ".join(wordlist[:amt_idx])
+        newline = guessAtSolution(wordlist)
+        print "i have a guess, and my guess is " + newline
+        ok = raw_input("was that right? Y/N ")
+        if ok != "Y":
+            newline = getUserInputSolution(wordlist)
 
-        unit_idx = int(raw_input("where does the unit break go? "))
-        unit = " ".join(wordlist[amt_idx:unit_idx])
-
-        ingredient_idx = int(raw_input("where does the ingredient break go? "))
-        ingredient = " ".join(wordlist[unit_idx:ingredient_idx])
-        newline = ",".join([amt, unit, ingredient])
-        print newline
         recipe_csv.write(newline + "\n")
 
     recipe_csv.close()
+
+def getUserInputSolution(wordlist):
+    amt_idx = int(raw_input("where does the amount break go? "))
+
+    amt = " ".join(wordlist[:amt_idx])
+
+    unit_idx = int(raw_input("where does the unit break go? "))
+    unit = " ".join(wordlist[amt_idx:unit_idx])
+
+    ingredient_idx = int(raw_input("where does the ingredient break go? "))
+    ingredient = " ".join(wordlist[unit_idx:ingredient_idx])
+    newline = ",".join([amt, unit, ingredient])
+    print newline
+    return newline
+
+def guessAtSolution(wordlist):
+    # get all words with numbers in it:
+    idx = 0
+    amt_words = []
+    while (wordlist[idx][0].isdigit()):
+        amt_words.append(wordlist[idx])
+        idx += 1
+    amt = " ".join(amt_words)
+
+    unit = wordlist[idx]
+    idx += 1
+    ingredient = " ".join(wordlist[idx:len(wordlist)])
+
+    return ",".join([amt, unit, ingredient])
+
+
+
 
 
 
